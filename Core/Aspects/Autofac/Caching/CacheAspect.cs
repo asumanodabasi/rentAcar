3fd1f4820d62ebbae12f25b517c351cb7f274efb
asumanodabasi <asumanodabasi1@gmail.com>
2023-03-04 16:33:56 +0300

    CoreModule added

---
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Aspects.Autofac.Caching
+{
+    public class CacheAspect
+    {
+
+    }
+}
---
+++ Core/CrossCuttingConserns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,49 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.CrossCuttingConserns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        IMemoryCache _memoryCache;
+
+        public MemoryCacheManager()
+        {
+            _memoryCache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+        }
+
+        public void Add(string key, object value, int duration)
+        {
+            _memoryCache.Set(key, value, TimeSpan.FromMinutes(duration));
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public bool IsAdd(string key)
+        {
+            return _memoryCache.TryGetValue(key, out _);
+        }
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+        }
+
+        public void RemovePattern(string pattern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
